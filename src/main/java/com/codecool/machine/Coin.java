e70743cfa1ed82f8b2c2d90b5475cbab2658fbef
MateuszGolda <mateusz.golda1993@gmail.com>
2020-10-06 13:06:39 +0200

    add enum for invalid coin

--- src/main/java/com/codecool/machine/Coin.java
+++ src/main/java/com/codecool/machine/Coin.java
@@ -3,7 +3,8 @@ package com.codecool.machine;
 public enum Coin {
     NICKEL(5, 500, 50, 5, 2),
     DIME(10, 1000, 100, 10, 4),
-    QUARTER(25, 2500, 250, 25, 10);
+    QUARTER(25, 2500, 250, 25, 10),
+    INVALID(-1,-1,-1,0,0);
 
     final int value;
     final int weight;
--- src/main/java/com/codecool/machine/CoinFactory.java
+++ src/main/java/com/codecool/machine/CoinFactory.java
@@ -3,16 +3,15 @@ package com.codecool.machine;
 import java.util.Optional;
 
 public class CoinFactory {
-    public Optional<Coin> getCoin(int weight, int size) {
-        Optional<Coin> optionalCoin = Optional.empty();
+    public Coin getCoin(int weight, int size) {
         for (Coin coin : Coin.values()) {
             if (weight >= coin.weight - coin.weightError
                     && weight <= coin.weight + coin.weightError
                     && size >= coin.size - coin.sizeError
                     && size <= coin.size + coin.sizeError) {
-                optionalCoin = Optional.of(coin);
+                return coin;
             }
         }
-        return optionalCoin;
+        return Coin.INVALID;
     }
 }
